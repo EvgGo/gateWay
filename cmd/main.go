914b4eaaa30f3db65209bf1ef620cce12bb7d8bd
Evgenii Kudinov <evgenii.kudinov1@gmail.com>
2026-04-06 12:16:27 +0300

    сделал точку запуска

---
+++ cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"gateWay/internal/config"
+	"gateWay/internal/grpc"
+	"gateWay/internal/routes"
+	"gateWay/pkg/logger"
+	authv1 "github.com/EvgGo/proto/proto/gen/go/sso"
+	workspacev1 "github.com/EvgGo/proto/proto/gen/go/teamAndProjects"
+	"github.com/joho/godotenv"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"google.golang.org/grpc"
+	"log"
+	"log/slog"
+	"net/http"
+)
+
+func main() {
+
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("Внимание: файл .env не найден, используются переменные окружения по умолчанию")
+		panic(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	cfg := config.MustLoad("CONFIG_PATH_GATEWAY")
+
+	l, err := logger.SetupLogger(cfg.Env, "", cfg.LogLevel, cfg.LogFile)
+	if err != nil {
+		log.Fatalf("Ошибка при инициализации логгера: %v", err)
+	}
+
+	userServiceConn, err := connection.ConnectWithRetry(ctx, cfg.Auth.Host, cfg.Auth.Port, cfg.DialConfig, l)
+	if err != nil {
+		l.Error("Error on connecting to the AuthProf-service:", err)
+		return
+	}
+
+	workspaceConn, err := connection.ConnectWithRetry(ctx, cfg.WorkSpace.Host, cfg.WorkSpace.Port, cfg.DialConfig, l)
+	if err != nil {
+		l.Error("Error on connecting to the AuthProf-service:", err)
+		return
+	}
+
+	defer func(userServiceConn *grpc.ClientConn) {
+		err = userServiceConn.Close()
+		if err != nil {
+			l.Warn("Unable to close connection AuthProf-service:", err)
+		}
+	}(userServiceConn)
+
+	authClient := authv1.NewAuthClient(userServiceConn)
+	profileClient := authv1.NewUserProfileClient(userServiceConn)
+	workspaceTeamsClient := workspacev1.NewTeamsClient(workspaceConn)
+	workspaceProjectsClient := workspacev1.NewProjectsClient(workspaceConn)
+	skillClient := authv1.NewSkillsClient(userServiceConn)
+
+	router := routes.NewRoutes(l, authClient, profileClient, workspaceTeamsClient, workspaceProjectsClient, skillClient)
+	router.HandleFunc("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8082/swagger/doc.json"), //The url pointing to API definition
+	))
+
+	l.Info("Starting server:", slog.String("address", cfg.HTTPServer.Address))
+	server := &http.Server{
+		Addr:              cfg.HTTPServer.Address,
+		Handler:           router,
+		ReadHeaderTimeout: cfg.HTTPServer.ReadTimeout,
+		WriteTimeout:      cfg.HTTPServer.WriteTimeout,
+		IdleTimeout:       cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err = server.ListenAndServe(); err != nil {
+		l.Error("failed to start server", "error", err)
+	}
+
+	l.Error("server stopped")
+}
